254c24fb298af0b59e10457836cc6f9e9db308c1
Maskgirl <tithimukherjee12@gmail.com>
2022-10-16 22:22:22 +0530

    Added basic django project

---
+++ cryptocurrency-price-tracker/settings/settings.py
@@ -0,0 +1,8 @@
+from .default import *
+
+COINS_MIN_MAX_MAP = {
+    "bitcoin": {
+        "min": 10,
+        "max": 20
+    }
+}
---
+++ cryptocurrency-price-tracker/utils.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def check_prices_runner():
+    print("calling....")
+
+
+def check_price_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(check_prices_runner, 'interval', seconds=5)
+    scheduler.start()
