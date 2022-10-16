e54417dc50f47d02f40002269e8dd730207d398e
Maskgirl <tithimukherjee12@gmail.com>
2022-10-16 22:26:05 +0530

    Renamed app, replaced - to _ and added new app price_tracker

--- cryptocurrency_price_tracker/wsgi.py
+++ cryptocurrency_price_tracker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cryptocurrency-price-tracker project.
+WSGI config for cryptocurrency_price_tracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cryptocurrency-price-tracker.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cryptocurrency_price_tracker.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "cryptocurrency-price-tracker.settings"
+        "DJANGO_SETTINGS_MODULE", "cryptocurrency_price_tracker.settings"
     )
     try:
         from django.core.management import execute_from_command_line
